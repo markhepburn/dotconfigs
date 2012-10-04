565482f0cb4751a90f167971f429d650988a04aa
Mark Hepburn <Mark.Hepburn@gmail.com>
2012-10-04 11:06:07 +1000

    Tweaking the script to accomodate files that live .config (more complicated because we need to create directories without over-writing, etc)

--- create_links.sh
+++ create_links.sh
@@ -8,6 +8,23 @@
 
 CURRENTDIR=`canonicalise $0`
 
+# For simple dot-files we just symbolically link them in $HOME:
 for dotfile in `ls -aF | grep -v '/$' | grep '^\.'`; do
     ln -si "${CURRENTDIR}/$dotfile" ~/$dotfile
+done
+
+# Directory-structured things are a bit more complicated; the main
+# culprit is things that live in ~/.config.  I don't want to keep
+# everything in .config versioned so I can't just link ~/.config in
+# here, so we do a bit of a dance of creating any non-existing
+# directory components, then linking files in here to the actual
+# directories in $HOME.
+for DIR in `find . -type d | grep -v .git | grep -v ^\.$`; do
+    mkdir -p ~/${DIR}
+done
+for DIR in `find . -maxdepth 1 -type d | grep -v .git | grep -v ^\.$`; do
+    for CONF in `find ${DIR} -type f`; do
+        BASEDIR=`dirname ${CONF}`
+        ln -si ${CURRENTDIR}/${CONF} ~/${BASEDIR}
+    done
 done
